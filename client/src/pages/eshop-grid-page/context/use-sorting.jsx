381a7fdbbb49640ebab8adf8218ef1eb31ff06de
Gerardas Ciegis <gerardasc@gmail.com>
2022-03-16 17:33:06 +0200

    bug fixes

--- client/src/pages/eshop-grid-page/context/use-sorting.jsx
+++ client/src/pages/eshop-grid-page/context/use-sorting.jsx
@@ -5,8 +5,8 @@ const useSorting = (setProducts) => {
 
   const mapSorting = {
     1: (products) => products.sort((a, b) => (a.id - b.id)),
-    2: (products) => products.sort((a, b) => (b.price - a.price)),
-    3: (products) => products.sort((a, b) => (a.price - b.price)),
+    2: (products) => products.sort((a, b) => (a.price - b.price)),
+    3: (products) => products.sort((a, b) => (b.price - a.price)),
   };
 
   useEffect(() => {
